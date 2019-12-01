fa0597bc7ccd625280287f1b9b114a75a30581ec
Malin Ortenblad <mo2792@columbia.edu>
2019-12-01 22:51:45 +0000

    Revert "Add map template and export mgmt command"
    This reverts commit 4c687b29241f6d97cb57ad6bcfbe15e89f9e9ff2.

--- squirrel/adopt/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Pet
-
-admin.site.register(Pet)
-
-
-
-# Register your models here.
--- squirrel/management/commands/import_squirrel_data.py
+++ squirrel/management/commands/import_squirrel_data.py
@@ -1,52 +1,20 @@
 import csv
 from django.core.management import BaseCommand
-from squirrel.models import Squirrel
-import pandas as pd
+from app.models import Question
 
 class Command(BaseCommand):
      help = "Import squirrel data."
 
      def add_arguments(self, parser):
-         parser.add_argument('csv_file', nargs='+', type=str)
-    
-    def handle(self, *args, **kwargs):
-         csv_file = args['csv_file']
-         dataReader = pd.read_csv(csv_file, encoding = 'latin1')
-         for index, row in dataReader.iterrows():
+         parser.add_argument('csvfile', nargs='?', type=argparse.FileType('r'))
 
-    # def handle(self, *args, **options):
-     #    for csv_file in options['csv_file']:
-      #       dataReader = pd.read_csv('csv_file', encoding = 'latin1')
-       #      for index, row in dataReader.iterrows():
-                 squirrel = Squirrel()
-                 squirrel.Latitude = row['X']
-                 squirrel.Longitude = row['Y']
-                 squirrel.Uniqure_Squirrel_ID = row['Unique Squirrel ID']
-                 squirrel.Shift = row['Shift']
-                 squirrel.Date = row['Date']
-                 # squirrel.Hector_squirrel_Number = row.get('Hector Squirrel Number')
-                 squirrel.Age = row['Age_Category']
-                 squirrel.Primary_Fur_Color = row['Primary Fur Color']
-                  # squirrel.Combination_of_Primary_and_Highlight_Color = row.get('Combination of Primary and Highkight Color')
-                 #squirrel.Color_notes = row.get('X')
-                 squirrel.Location = row['Location']
-                 #squirrel.Above_Ground_Sighter_Measurement = row.get('X')
-                 squirrel.Specific_Location = row['Specific Location']
-                 squirrel.Running = row['Running']
-                 squirrel.Chasing = row['Chasing']
-                 squirrel.Climing = row['Climing']
-                 squirrel.Eating = row['Eating']
-                 squirrel.Foraging = row['Foraging']
-                 squirrel.Other_Activities = row['Other']
-                 squirrel.Kuks = row['Kuks']
-                 squirrel.Quaas = row['Quaas']
-                 squirrel.Moans = row['Moans']
-                 squirrel.Tail_flags = row['Tail flags']
-                 squirrel.Tail_twitches = row['Tail twitches']
-                 squirrel.Approaches = row['Approaches']
-                 squirrel.Indifferent = row['Indifferent']
-                 squirrel.Runs_from = row['Runs from']
-                # squirrel.Other_Interactions = row.get('X')
-                # squirrel.LatLong = row.get('X')
-                 squirrel.save() 
+     def handle(self, *args, **options):
+         squirrel_data = []
+         sq = Squirrel.objects.get(name=homeroom)
+         with options['csvfile'] as csvfile:
+             reader = csv.DictReader(csvfile)
+             for row in reader:
+                 student_list.append(Student(name=row["Student Name"], studentidnum=row["School ID"], homeroom=hr))
+
+
 
