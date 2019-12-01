d1a846879efda6cc5e1b3294c9c47720e7d91693
Mihir Ahuja <ma3973@columbia.edu>
2019-12-01 23:08:44 +0000

    Add sighting view

--- squirrel/views.py
+++ squirrel/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Squirrels
 
+def sightings(request):
+    all_squirrels = Squirrels.objects.all()
+    return render(request,'squirrel/sightings.html',locals())
 # Create your views here.
