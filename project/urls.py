742a06296e3e5059afcd6863b2361facb225e5d9
Ortenblad <56941065+Ortenblad@users.noreply.github.com>
2019-12-03 18:09:45 -0500

    Update urls

--- project/urls.py
+++ project/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-        path('', include('squirrel.urls')),
+        path('', include('squirrel.urls'))
         #path('adopt/', include('adopt.urls')),
         path('admin/', admin.site.urls),
 ]
--- squirrel/urls.py
+++ squirrel/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('sightings/',views.sightings),
+    path('', views.sightings, name='sightings'),
     path('<int:unique_squirrel_id>/', views.sightings_details),
     path('sightings/add/',views.sightings_add),
     path('map/',views.map),
