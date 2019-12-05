e714975596d55813678bb4f15eb67bf504d638b3
Ortenblad <56941065+Ortenblad@users.noreply.github.com>
2019-12-05 13:09:10 -0500

    Update stats view and html

--- squirrel/views.py
+++ squirrel/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Sightings
 from .forms import SightingsForm
 from django.core.exceptions import MultipleObjectsReturned
+from django.db.models import Count
 
 
 from django.http import HttpResponse
@@ -47,6 +48,21 @@ def sightings_stats(request):
     #        'all_sightings': all_sightings,
     #}
     return render(request,'sightings/stats.html',context)
+
+def sightings_stats(request):
+    squirrel_age = Sightings.objects.values('age').order_by('age').annotate(age_count=Count('age'))
+    squirrel_primary_fur_color = Sightings.objects.values('primary_fur_color').order_by('primary_fur_color').annotate(primaryfurcolor_count=Count('primary_fur_color'))
+    squirrel_climbing =  Sightings.objects.values('climbing').order_by('climbing').annotate(climbing_count=Count('climbing'))
+    squirrel_chasing = Sightings.objects.values('chasing').order_by('chasing').annotate(chasing_count=Count('chasing'))
+    squirrel_running = Sightings.objects.values('running').order_by('running').annotate(running_count=Count('running'))
+    context = {
+        'squirrel_age': squirrel_age,
+        'squirrel_primary_fur_color': squirrel_primary_fur_color,
+        'squirrel_climbing': squirrel_climbing,
+        'squirrel_chasing': squirrel_chasing,
+        'squirrel_running': squirrel_running,
+    }
+    return render(request, 'sightings/stats.html', context)
 
 def map(request):
     all_sightings = Sightings.objects.all()[:100]
