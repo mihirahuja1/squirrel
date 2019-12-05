c001ce9a9d9e387cb0d121dbc3f182dd7edbdc5c
mihirahuja1 <ma3973@columbia.edu>
2019-12-05 15:42:12 -0500

    Format file and correct export function

--- squirrel/management/commands/export_squirrel_data.py
+++ squirrel/management/commands/export_squirrel_data.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from tracker.models import Sightings
+from squirrel.models import Sightings
 import csv
 
 class Command(BaseCommand):
@@ -11,7 +11,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         path = options['path']
         sightings = Sightings.objects.all()
-        fields = [field.name for field in Sighting._meta.fields]
+        fields = [field.name for field in Sightings._meta.fields]
         with open(path, mode='w') as csvfile:
             writer = csv.writer(csvfile, delimiter=',')
             writer.writerow(fields)
